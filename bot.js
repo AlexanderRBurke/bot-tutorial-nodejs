0087a3ddbc06f4806b37b1e2cbf4271874f53a88
AlexanderRBurke <AlexanderRBurke@gmail.com>
2025-02-01 09:28:15 -0800

    Add phrases

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ var cool = require('cool-ascii-faces');
 var botID = "";
 
 const phrases = {
+  0: "RoN?",
   1: "Yes.",
   2: "No.",
   3: "Maybe.",
@@ -103,10 +104,12 @@ const phrases = {
   97: "Orange",
   98: "Aaaaagh! The Humanity!",
   99: "Let me get that for you.",
-  100: "They keep on rising!"
+  100: "They keep on rising!",
+  101: "I am down, with a squad.",
+  144: "Have you fixed your little cock yet?"
 };
 
-const regex = /\b(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60|61|62|63|64|65|66|67|68|69|70|71|72|73|74|75|76|77|78|79|80|81|82|83|84|85|86|87|88|89|90|91|92|93|94|95|96|97|98|99|100)\b/g;
+const regex = /\b(0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60|61|62|63|64|65|66|67|68|69|70|71|72|73|74|75|76|77|78|79|80|81|82|83|84|85|86|87|88|89|90|91|92|93|94|95|96|97|98|99|100|101|144|169)\b/g;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
@@ -126,7 +129,11 @@ function respond() {
 function postMessage(matches) {
   var botResponse, options, body, botReq;
 
-  botResponse = phrases[matches[0]]; //cool();
+  if (matches[0] == 169) {
+    botResponse = cool();
+  } else {
+    botResponse = phrases[matches[0]];
+  }
 
   options = {
     hostname: 'api.groupme.com',
