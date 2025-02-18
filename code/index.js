37617bb75bb1372187985c579a75e6fc2882c81c
AlexanderRBurke <AlexanderRBurke@gmail.com>
2025-02-18 10:10:18 -0800

    Refactor code into code folder

--- code/index.js
+++ code/index.js
@@ -29,7 +29,7 @@ port = Number(process.env.PORT || 5000);
 server.listen(port, "0.0.0.0");
 
 function ping() {
-  const data = fs.readFileSync("./index.html");
+  const data = fs.readFileSync("./code/index.html");
 
   if (!data) {
     this.res.writeHead(500, { "Content-Type": "text/plain" });
